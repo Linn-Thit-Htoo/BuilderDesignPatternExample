0f9817a569e303b1093465803e4b02ed9cd0c831
Linn Thit <lth1212001@gmail.com>
2024-05-21 01:02:39 +0630

    Add project files.

---
+++ BuilderDesignPatternExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var computer = new ComputerBuilder()
+            .AddCpu("Intel i7")
+            .AddRam("8")
+            .AddHardDisk("1 TB")
+            .Build();
+
+        Console.WriteLine(JsonConvert.SerializeObject(computer));
+    }
+}
+
+public class Computer
+{
+    public string CPU { get; set; } = null!;
+    public string RAM { get; set; } = null!;
+    public string HardDisk { get; set; } = null!;
+}
+
+public class ComputerBuilder
+{
+    private Computer _computer = new();
+
+    public ComputerBuilder AddRam(string ram)
+    {
+        _computer.RAM = ram;
+        return this;
+    }
+
+    public ComputerBuilder AddCpu(string cpu)
+    {
+        _computer.CPU = cpu;
+        return this;
+    }
+
+    public ComputerBuilder AddHardDisk(string hardDisk)
+    {
+        _computer.HardDisk = hardDisk;
+        return this;
+    }
+
+    public Computer Build()
+    {
+        return _computer;
+    }
+}
